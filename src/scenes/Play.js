cf8816ed29c4b2af62bf79f7eb9799cec8592729
Jonathan Alvarez <jalva100@ucsc.edu>
2022-04-09 20:44:50 -0700

    adding ting

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,11 +10,11 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         // green UI background 
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
-        // place tile sprite
-        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
@@ -29,5 +29,6 @@ class Play extends Phaser.Scene {
 
     update() {
         this.starfield.tilePositionX -= 4;
+        //this.p1Rocket.update();
       }
 }
